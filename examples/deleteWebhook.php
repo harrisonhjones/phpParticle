fdc0b440a5f9c46bb85955dffac6f7595a381617
Harrison Jones <harrisonhjones@gmail.com>
2015-03-21 15:20:17 -0700

    Added Webhook ID placeholder

--- examples/deleteWebhook.php
+++ examples/deleteWebhook.php
@@ -25,7 +25,7 @@ $spark->setDebugType("HTML");
 // Set our access token (set in the phpConfig.config.php file)
 $spark->setAccessToken($accessToken);
 
-$webhookID = '550de6ab239ed8a973c670ac';
+$webhookID = 'WEBHOOK_ID';
 // Delete spark webhook
 $spark->debug("Delete Spark Web Hook");
 if($spark->deleteWebhook($webhookID) == true)
